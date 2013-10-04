5ad72b855062d3db284fec06f0f4aabb4dc6c422
Daniel Herzog <alu0100699494@ull.edu.es>
2013-10-04 20:16:08 +0100

    Añadidas funciones para leer matrices del teclado

--- matrices.rb
+++ matrices.rb
@@ -3,6 +3,42 @@ $a = []
 # Matriz B(NxM)
 $b = []
 
+def asignar_matriz_teclado(a, filas, columnas)
+	i = 0
+	while( i < filas ) 
+		j = 0
+		fila = []
+		while(j < columnas)
+			print "Inserte elemento ", i,",", j, ": "
+			fila << gets.to_i
+			j += 1
+		end
+		a << fila
+		i += 1
+	end
+	yield (a)
+end
+
+def leer_teclado
+	print "Inserte numero de filas: "
+	while(	(filas = gets.to_i) < 1)
+		print "Error, intentelo de nuevo: "
+	end
+	
+	print "Inserte numero de columnas: "
+	while(	(columnas = gets.to_i) < 1)
+		print "Error, intentelo de nuevo: "
+	end
+	
+	print $a[0].class
+
+	puts "Elementos de la primera matriz"
+	asignar_matriz_teclado($a, filas, columnas) {|a| p a}
+	
+	puts "Elementos de la segunda matriz"
+	asignar_matriz_teclado($b, filas, columnas) {|a| p a}
+end
+
 seleccionado = false
 
 while(!seleccionado)
@@ -18,7 +54,7 @@ end
 
 print "Ha seleccionado la opcion ", entrada
 if(entrada == "teclado\n")
-    # Leer datos de teclado
+    leer_teclado
 else
 	# Leer datos de fichero
 end
