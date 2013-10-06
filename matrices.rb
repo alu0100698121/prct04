6b2039edf5c418a75cb7c0b0c6b3e88bc659e8cf
Eliasib Garcia <alu0100698121@ull.edu.es>
2013-10-06 16:57:14 +0100

    añadida funciones de lectura de ficheros

--- matrices.rb
+++ matrices.rb
@@ -3,6 +3,50 @@ $a = []
 # Matriz B(NxM)
 $b = []
 
+def asignar_matriz_fichero(fichero, a, filas)
+	#fichero = File.new(ARGV[0],"r")
+	#if(fichero == nil)
+	#	print "Error cargando ficehro ", ARGV[0]
+	#	return
+	#end
+
+	i = 0
+	while(i < filas)
+		linea = fichero.gets
+		elementos = linea.split(' ')
+		a[i] << elementos.each.to_f
+		i += 1
+	end
+	
+	yield(a)
+end
+
+def leer_fichero
+	
+	fichero = nil
+	while(fichero == nil)
+		print "Introduzca el nombre del fichero para cargar las matrices: "
+		nombre_fichero = gets
+		fichero = File.new(nombre_fichero, "r")
+		if(fichero == nil)
+			puts "Fichero invalido"
+		end
+	end
+
+	filas = fichero.gets.to_i
+	if(filas < 1)
+		puts "Error: la matriz no puede tener menos de 1 fila"
+		return
+	end
+
+	asignar_matriz_fichero(fichero, $a, filas)
+	asignar_matriz_fichero(fichero, $b, filas)
+
+	fichero.close
+
+end
+	
+
 seleccionado = false
 
 while(!seleccionado)
@@ -20,5 +64,5 @@ print "Ha seleccionado la opcion ", entrada
 if(entrada == "teclado\n")
     # Leer datos de teclado
 else
-	# Leer datos de fichero
+	leer_fichero
 end
